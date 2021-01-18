8e419d98af37ea93a36342bd40402ffec30eefb2
Pranaya Anargya <panargya@gmail.com>
2021-01-18 20:23:32 +1100

    fixed skill grid

--- src/Components/SkillSection/SkillData.js
+++ src/Components/SkillSection/SkillData.js
@@ -2,6 +2,7 @@ import html from '../Images/GridImages/html.jpg'
 export const SkillData = [
     
     {
+        id:0,
         image: html,
         alt: 'illustration here',
         title: 'HTML',
@@ -9,42 +10,49 @@ export const SkillData = [
     },
     {
         
+        id:1,
         image: null,
         alt: 'illustration here',
         title: 'CSS',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:2,
         image: null,
         alt: 'illustration here',
         title: 'JavaScript',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:3,
         image: null,
         alt: 'illustration here',
         title: 'React Framework',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:4,
         image: null,
         alt: 'illustration here',
         title: 'SQL',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:5,
         image: null,
         alt: 'illustration here',
         title: 'Java',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:6,
           image: null,
         alt: 'illustration here',
         title: 'Website deployment',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nobis quaerat expedita libero ea labore cumque similique distinctio sed minus laborum, excepturi consequatur perspiciatis accusantium fugit corporis vel voluptas accusamus.'
     },
     {
+        id:7,
         image: null,
         alt: 'illustration here',
         title: 'Gimp',
--- src/Components/SkillSection/SkillSection.js
+++ src/Components/SkillSection/SkillSection.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import React, {useState} from 'react'
 import { SkillData } from './SkillData';
 import { GridSection, GridContainer, GridButtonWrapper, GridHeroWrapper, GridHeroImg, GridText, GridItemContainer, GridMainWrapper, GridTextContent, GridTextTitle, Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7, Logo8, GridHeader, GridHeadTitle, GridTextParent} from "./SkillSectionStyles";
@@ -7,23 +8,11 @@ import { GridSection, GridContainer, GridButtonWrapper, GridHeroWrapper, GridHer
 const SkillSection = () => {
 
     const [val, setVal] = useState(0);
-    const [test, setDisplay] = useState(false)
     
-    // const showElement = () => {
+    
 
-    //     return(
-            
-    //     <GridTextParent>
-
-    //         <GridHeroImg />
-
-    //         <GridText>
-                    
-    //         </GridText>
-
-    //     </GridTextParent>
-    //     )
-    // }
+    
+    
     
 
 
@@ -43,8 +32,8 @@ const SkillSection = () => {
 
                         <GridItemContainer >
 
-                           <Logo1  onClick={() => setVal(0) && setDisplay(true)}/>
-                           <Logo2  onClick={() => setVal(1) && setDisplay(true)}/>
+                           <Logo1  onClick={() => setVal(0)}/>
+                           <Logo2  onClick={() => setVal(1)}/>
                            <Logo3  onClick={() => setVal(2)}/>
                            <Logo4  onClick={() => setVal(3)}/>
                            <Logo5  onClick={() => setVal(4)}/>
@@ -56,42 +45,35 @@ const SkillSection = () => {
                 </GridButtonWrapper>
                 
                 
-                {/* <GridHeroWrapper>
+                
+                 <GridHeroWrapper>
+                     {/* {SkillData.map((item, i ) => {
+                         if(i === val){
+                             return (
+                                 <div >
+                                     {item.title}
+                                 </div>
+                             )
+                         }
+                     })} */}
 
-                    <GridTextParent>
+                     {SkillData.filter(idx => idx.id === val).map((item,index) => (
+                         <div key={index}>
+                             {item.title}
+                         </div>
+                     ))}
+
+                    {/* <GridTextParent>
                         <GridHeroImg />
                         <GridText>
                             
                         </GridText>
-                    </GridTextParent>
+                    </GridTextParent> */}
 
-                </GridHeroWrapper> */}
-                        {SkillData.map((item, index) => {
-                            return(
-                        <GridHeroWrapper key={index}>
-                            {index === val && (
-                                <GridTextParent test={test}>
+                </GridHeroWrapper> 
 
-                                    <GridHeroImg  alt={item.alt} />
-
-                                    <GridText>
-
-                                        <GridTextTitle>
-                                            {item.title}
-                                        </GridTextTitle>
-
-                                        <GridTextContent>
-                                            {item.text}
-                                        </GridTextContent>
-
-                                    </GridText>
-
-                                </GridTextParent>
-                            )}
-
-                        </GridHeroWrapper>
-                            );
-                        })}
+                
+                        
                 </GridMainWrapper>
             </GridContainer>
         </GridSection>
